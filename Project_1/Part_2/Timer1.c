54934b2a90eb98403abbc1e7586be7cfc175e7ed
ChaoticNerd <nat.f.kho@gmail.com>
2025-08-25 16:50:53 -0700

    Updated P1P2
    Handler never triggered

---
+++ Project_1/Part_2/Timer1.c
@@ -0,0 +1,31 @@
+#include "tm4c123gh6pm.h"
+
+// ***************** Timer1_Init ****************
+// Activate TIMER1 interrupts to run user task periodically
+// Inputs:  task is a pointer to a user function
+//          period in units (1/clockfreq)
+// Outputs: none
+
+#define RED_LED       (*((volatile unsigned long *)0x40025008))
+#define RED_LED_MASK    0x02  // bit pisition for onboard blue LED
+
+void Timer1A_Init(unsigned long period){
+  SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
+  TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
+  TIMER1_CFG_R = 0x00000100;    // 2) configure for 16-bit mode
+  TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic down-count mode
+  TIMER1_TAILR_R = period-1;    // 4) reload value
+  TIMER1_TAPR_R = 8;            // 5) bus clock prescale
+  TIMER1_ICR_R = 0x00000010;    // 6) clear TIMER0A timeout flag
+  TIMER1_IMR_R = 0x00000010;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0xFF1FFFFF)|0x0000A000; // 8) enable priority 5
+// interrupts enabled in the main program after all devices initialized
+// vector number 35, interrupt number 19
+  NVIC_EN0_R = 1<<21;           // 9) enable IRQ 19 in NVIC
+  TIMER1_CTL_R = 0x00000010;    // 10) enable TIMER0A
+}
+
+void Timer1A_Handler(void){
+	RED_LED ^= RED_LED_MASK; // toggle PF1: Red LED
+  TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER0A timeout
+}
---
+++ Project_1/Part_2/main.c
@@ -0,0 +1,38 @@
+#include "tm4c123gh6pm.h"
+#include "SysTick.h"
+#include "PLL.h"
+#include "Timer1.h"
+
+#define RED_LED       (*((volatile unsigned long *)0x40025008))
+#define RED_LED_MASK    0x02  // bit pisition for onboard blue LED
+
+void PORTF_Init(void);
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void WaitForInterrupt(void);  // low power mode
+
+int main(void){
+	DisableInterrupts();
+	PORTF_Init();							// PF2(Blue LED) is an output for debugging
+  PLL_Init();               // set system clock to 50 MHz
+  //SysTick_Init();           // initialize SysTick timer
+	Timer1A_Init(40000000);		// initialize timer1 (1 Hz)
+	EnableInterrupts();
+	RED_LED = 0x00;
+	
+  while(1){
+    //SysTick_Wait10ms(25);    // approximately 10*10 ms = 0.1s
+		WaitForInterrupt();
+  }
+}
+
+void PORTF_Init(void){
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5; // activate port F
+	while ((SYSCTL_RCGCGPIO_R&SYSCTL_RCGCGPIO_R5)!=SYSCTL_RCGCGPIO_R5){};
+	
+	GPIO_PORTF_DIR_R |= RED_LED_MASK; // make PF2 out (built-in blue LED)
+  GPIO_PORTF_AFSEL_R &= ~RED_LED_MASK;// disable alt funct on PF2
+  GPIO_PORTF_DEN_R |= RED_LED_MASK; // enable digital I/O on PF2                            
+  GPIO_PORTF_PCTL_R &= ~0x000000F0; // configure PF2 as GPIO
+  GPIO_PORTF_AMSEL_R |= RED_LED_MASK;   // disable analog functionality on PF  
+}
