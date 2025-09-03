a6f60c9da580ea64dbbabc13ca0dab68bc9b06a3
ChaoticNerd <nat.f.kho@gmail.com>
2025-09-03 11:33:25 -0700

    h

--- Project_1/Part_3/Timer1.c
+++ Project_1/Part_3/Timer1.c
@@ -7,7 +7,7 @@
 // Outputs: none
 
 #define RED_LED       (*((volatile unsigned long *)0x40025008))
-#define RED_LED_MASK    0x02  // bit pisition for onboard blue LED
+#define RED_LED_MASK    0x02  // bit position for onboard blue LED
 
 void Timer1A_Init(unsigned long period){
   SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
@@ -15,10 +15,10 @@ void Timer1A_Init(unsigned long period){
   TIMER1_CFG_R = 0x00000004;    // 2) configure for 16-bit mode
   TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic down-count mode
   TIMER1_TAILR_R = period-1;    // 4) reload value
-  TIMER1_TAPR_R = 255;          // 5) bus clock prescale to 255
+  TIMER1_TAPR_R = 0xFF;          // 5) bus clock prescale to 255
   TIMER1_ICR_R = 0x00000001;    // 6) clear TIMER0A timeout flag
   TIMER1_IMR_R = 0x00000001;    // 7) arm timeout interrupt
-  NVIC_PRI5_R = (NVIC_PRI5_R&0xFF1FFFFF)|0x0000A000; // 8) enable priority 5
+  NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF1FFF)|0x0000A000; // 8) enable priority 4
 // interrupts enabled in the main program after all devices initialized
 // vector number 35, interrupt number 19
   NVIC_EN0_R = 1<<21;           // 9) enable IRQ 19 in NVIC
