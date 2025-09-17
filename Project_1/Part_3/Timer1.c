241232c0804a3314cfb8811d019514ebbf03a16a
JustHiro <justin.narciso@gmail.com>
2025-09-16 22:54:37 -0700

    P1P3 almost done,
    P1P3 is almost complete edited, switched timings around so it can properly produce a trigger signal, and rewrote function for timer1a_start

--- Project_1/Part_3/Timer1.c
+++ Project_1/Part_3/Timer1.c
@@ -7,19 +7,20 @@
 // Inputs:  task is a pointer to a user function
 //          period in units (1/clockfreq)
 // Outputs: none
-#define ECHO_PIN                             (((volatile unsigned long)0x40005100))
-#define TRIGGER_PIN                         (((volatile unsigned long)0x40005200))
+#define ECHO_PIN                            (*((volatile unsigned long*)0x40005100))
+#define TRIGGER_PIN                         (*((volatile unsigned long*)0x40005200))
 
 #define TRIGGER_VALUE     									0x80   // trigger at bit 7
 #define ECHO_VALUE             							0x40   // trigger at bit 6
 
-void Timer1A_Init(unsigned long period, unsigned long setTAMR){
+void Timer1A_Init(unsigned long period){
   SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
+	
   TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
   TIMER1_CFG_R = 0x00000004;    // 2) configure for 16-bit mode
-  TIMER1_TAMR_R = setTAMR;			// 3) configure for periodic down-count or one-shot depending on setTAMR
+  TIMER1_TAMR_R = 0X00000002;			// 3) configure for periodic down-count or one-shot depending on setTAMR
   TIMER1_TAILR_R = period-1;    // 4) reload value
-  TIMER1_TAPR_R = 0x63;         // 5) bus clock prescale to 255
+  TIMER1_TAPR_R = 0x07;         // 5) bus clock prescale to 255
   TIMER1_ICR_R = 0x00000001;    // 6) clear TIMER1A timeout flag
   TIMER1_IMR_R = 0x00000001;    // 7) arm timeout interrupt
   NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF1FFF)|0x0000A000; // 8) enable priority 4
@@ -30,27 +31,34 @@ void Timer1A_Init(unsigned long period, unsigned long setTAMR){
   TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
 }
 
-long Timer1A_Stop(void){
+void Timer1A_Stop(void){
 	// Stop Timer1A
-	uint32_t timeProgressed =(TIMER1_TAILR_R - TIMER1_TAR_R) *(TIMER1_TAPR_R + 1);
-	TIMER1_TAR_R = 0x00000000;		// Resets current time
 	TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
-	return timeProgressed;
+	
 }
-void Timer1A_OneShot(void){
+
+void Timer1A_Start(void){
+
+	//uint32_t timeProgressed =(TIMER1_TAILR_R - TIMER1_TAR_R) *(TIMER1_TAPR_R + 1);
+	TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
+	TIMER1_TAILR_R = 0;
+	
+	TIMER1_TAR_R = 0x00000000;		// Resets current time
+	
+	TIMER1_CTL_R = 0x00000001;
+	
+}
+
+void Timer1A_Periodic(void){
 		TRIGGER_PIN &= ~TRIGGER_VALUE; // send low to trigger
 	
-		Timer1A_Init(121600, 0x00000001); // 2us down
+		Timer1A_Init(121600); // 2us down
 		while(TIMER1_TAR_R != 0);
+		Timer1A_Stop();
 		TRIGGER_PIN |= TRIGGER_VALUE; // send high to trigger
 		
-		Timer1A_Init(608000, 0x00000001); // 10us up
+		Timer1A_Init(608000); // 10us up
 		while(TIMER1_TAR_R != 0);
+		Timer1A_Stop();
 		TRIGGER_PIN &= ~TRIGGER_VALUE; // send low to trigger
 }
-/* LED CODE FOR PART 4 PICKUP
-void Timer1A_Handler(void){	
-	RED_LED ^= RED_LED_MASK; // toggle PF1: Red LED
-	TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER1A timeout
-}
-*/
---
+++ Project_1/Part_3/Ultrasonic_Sensor.c
@@ -0,0 +1,64 @@
+// Ultrasonic_Sensor.c
+// This program runs on TM4C123.
+// PB6 connects to echo pin to generate edge-triggered interrupt.
+// PB7 connects to Ultrasonic sensor trigger pin.
+// GPTM Timer is used to generate the required timing for trigger pin and measure echo pulse width.
+// Ana Arante, Hanna Estrada, Emily Hsu, Suphia Sidiqi
+// September 8, 2025
+
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+uint32_t distance;
+
+#define TRIGGER_PIN 			(*((volatile unsigned long *)0x40005200))  // PB7 is the trigger pin	
+#define ECHO_PIN 					(*((volatile unsigned long *)0x40005100))  // PB6 is the echo pin	
+#define TRIGGER_VALUE 		0x80   				// trigger at bit 7
+#define ECHO_VALUE 				0x40   				// echo at bit 6
+#define PERIODIC					0x00000002		// bit position for periodic count-down mode
+#define MC_LEN 						0.0625 				// length of one machine cycle in microsecond for 16MHz clock
+#define SOUND_SPEED 			0.0343 				// centimeter per micro-second
+#define PERIODIC_RELOAD		38000					// time from sending ultrasonic to returning 
+
+void PortB_Init(void);
+extern void GPIOPortB_Handler(void);
+extern void Timer1A_Init(unsigned long period);
+extern uint32_t Timer1A_Stop(void);
+
+void PortB_Init(void){ 
+  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOB;     // 1) activate clock for Port B
+	while ((SYSCTL_RCGC2_R&SYSCTL_RCGC2_GPIOB)!=SYSCTL_RCGC2_GPIOB){}; // wait for clock to start
+
+	GPIO_PORTB_PCTL_R &= ~0xFF000000; 				// 3) regular GPIO
+  GPIO_PORTB_AMSEL_R &= (uint32_t)~0xC0;    // 4) disable analog function on PA2
+  GPIO_PORTB_DIR_R &= ~0x40;        				// 5) PB6:echo pin, input
+  GPIO_PORTB_DIR_R |= 0x80;         				// 5) PB7:trigger pin, output
+  GPIO_PORTB_AFSEL_R &= ~0xC0;      				// 6) regular port function
+  GPIO_PORTB_DEN_R |= 0xC0;         				// 7) enable digital port
+  GPIO_PORTB_IS_R &= ~0x40;         				// PB6 is edge-sensitive
+  GPIO_PORTB_IBE_R |= 0x40;         				// PB6 is both edges
+  GPIO_PORTB_IEV_R &= ~0x40;        				// PB6 both edge event
+  GPIO_PORTB_ICR_R = 0x40;          				// clear flag 6
+  GPIO_PORTB_IM_R |= 0x40;          				// arm interrupt on PB6
+  NVIC_PRI0_R = (NVIC_PRI0_R&0xFFFF1FFF)|0x0000A000; // (g) priority 3
+  NVIC_EN0_R = 0x00000002;          				// (h) enable Port B edge interrupt
+}
+/*
+void GPIOPortB_Handler(void){
+	if (ECHO_PIN==ECHO_VALUE){  				// echo pin rising edge is detected, start timing
+			Timer1A_Init(38000);   // configure for periodic down-count mode
+	}
+	else { 
+		// echo pin falling edge is detected, end timing and calculate distance.
+    // The following code is based on the fact that the HCSR04 ultrasonic sensor 
+    // echo pin will always go low after a trigger with bouncing back
+    // or after a timeout. The maximum distance can be detected is 400cm.
+		// The speed of sound is approximately 340 meters per second, 
+		// or  .0343 c/µS.
+    // Distance = (echo pulse width * 0.0343)/2; = ((# of mc)*MC_LEN*SOUND_SPEED)/2
+		Timer1A_Stop();
+		//while(!ECHO_PIN);
+		distance = (uint32_t)(Timer1A_Stop()*MC_LEN*SOUND_SPEED)/2;		
+	}
+  GPIO_PORTB_ICR_R = 0x40;      // acknowledge flag 6
+}*/
